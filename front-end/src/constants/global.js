03ec25351ad25f6f91034fa7248e0e6abbf11df7
laminhtam <laminhtam5523@gmail.com>
2024-05-24 12:42:09 +0700

    update constant

--- front-end/src/constants/global.js
+++ front-end/src/constants/global.js
@@ -1,5 +1,5 @@
-export const COOKIE_DOMAIN = "localhost";
+export const COOKIE_DOMAIN = "localhost:3000";
 
-export const WAVE_CHAT_API_URL = "http://localhost:3000";
+export const WAVE_CHAT_API_URL = `http://${COOKIE_DOMAIN}`;
 
-export const WAVE_CHAT_SOCKET_URL = "ws://localhost:3000";
+export const WAVE_CHAT_SOCKET_URL = `ws://${COOKIE_DOMAIN}`;
